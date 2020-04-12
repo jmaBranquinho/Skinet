e5ea510d999f48e4448804e7707eb26b851a681f
jmabranquinho <jmabranquinho@gmail.com>
2020-04-12 19:58:12 +0100

    Added new middleware for error handling

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,42 @@
+﻿using API.Errors;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        private readonly StoreContext _context;
+
+        public BuggyController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("notfound")]
+        public ActionResult GetNotFoundRequest()
+        {
+            return NotFound(new ApiResponse(404));
+        }
+
+        [HttpGet("servererror")]
+        public ActionResult GetServerError()
+        {
+            var noProduct = _context.Products.Find(-1).ToString();
+
+            return Ok();
+        }
+
+        [HttpGet("badrequest")]
+        public ActionResult GetBadRequest()
+        {
+            return BadRequest(new ApiResponse(400));
+        }
+
+        [HttpGet("badrequest/{id}")]
+        public ActionResult GetNotFoundRequest(int id)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ API/Errors/ApiValidationErrorResponsive.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace API.Errors
+{
+    public class ApiValidationErrorResponsive : ApiResponse
+    {
+        public IEnumerable<string> Errors { get; set; }
+
+        public ApiValidationErrorResponsive() : base(400)
+        {
+        }
+    }
+}
