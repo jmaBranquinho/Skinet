aaa68592e115a029c807aa9f68c8c4aa281de73c
jmabranquinho <jmabranquinho@gmail.com>
2020-04-21 21:33:46 +0100

    Added toastr for validation errors and improved the error handling

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -1,18 +1,22 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { TestErrorComponent } from './test-error/test-error.component';
+import { ToastrModule } from 'ngx-toastr';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ServerErrorComponent } from './server-error/server-error.component';
-
+import { TestErrorComponent } from './test-error/test-error.component';
 
 
 @NgModule({
   declarations: [NavBarComponent, TestErrorComponent, NotFoundComponent, ServerErrorComponent],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true
+    })
   ],
   exports: [
     NavBarComponent
--- client/src/app/core/interceptors/error.interceptor.ts
+++ client/src/app/core/interceptors/error.interceptor.ts
@@ -1,12 +1,13 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
+import { NavigationExtras, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private toastr: ToastrService) {}
 
   intercept(
     req: HttpRequest<any>,
@@ -15,13 +16,26 @@ export class ErrorInterceptor implements HttpInterceptor {
     return next.handle(req).pipe(
       catchError((error) => {
         if (error) {
+          if (error.status === 400) {
+            if (error.error.errors) {
+              throw error.error;
+            } else {
+              this.toastr.error(error.error.message, error.error.statusCode);
+            }
+          }
+          if (error.status === 401) {
+            this.toastr.error(error.error.message, error.error.statusCode);
+          }
           if (error.status === 404) {
             this.router.navigateByUrl('/not-found');
           }
           if (error.status === 500) {
-                this.router.navigateByUrl('/server-error');
+            const navigationExtras: NavigationExtras = {
+              state: { error: error.error },
+            };
+            this.router.navigateByUrl('/server-error', navigationExtras);
           }
-          }
+        }
         return throwError(error);
       })
     );
